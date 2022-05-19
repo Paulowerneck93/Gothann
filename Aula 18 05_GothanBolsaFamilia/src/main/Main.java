dc9745c7c832dad85b381a7ef5249ff6903925c6
Paulowerneck93 <80072894+Paulowerneck93@users.noreply.github.com>
2022-05-18 21:36:29 -0300

    Add files via upload

---
+++ Aula 18 05_GothanBolsaFamilia/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import cadastro.Vilao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		Vilao vilaoMestre = new Vilao();
+		vilaoMestre.nome = "Coringa";
+		
+		System.out.println(vilaoMestre.nome);
+
+	}
+
+}
